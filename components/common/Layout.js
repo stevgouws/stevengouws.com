d829b56c03f33df3d7f63ff62fa24ea67f270f8f
Steven Gouws <steven@stevengouws.com>
2023-11-03 11:37:11 +0000

    Add meta description and clean up placement of HEAD

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,43 +1,31 @@
-import Head from "next/head";
 import { any } from "prop-types";
 export default function Layout({ children }) {
   return (
-    <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <title>Steven Gouws - Javascript Developer</title>
-      </Head>
-      <main className="container mx-auto lg:max-w-screen-A4 print:max-w-screen-A4 mx-w-xl bg-white shadow-xl print:shadow-none shadow-2xl p-8 md:p-12 print:py-0 rounded-md lg:rounded-sm">
-        {children}
-        <style jsx global>{`
-          @media screen {
-            html {
-              padding-top: 56px;
-              background: linear-gradient(
-                0deg,
-                hsl(189.88deg 86.31% 62.75%) 0%,
-                hsl(162.48deg 100% 82.02%) 66.66%,
-                hsl(189.88deg 86.31% 62.75%) 100%
-              );
-              height: 200%;
-            }
-            body {
-              overflow: scroll;
-            }
+    <main className="container mx-auto lg:max-w-screen-A4 print:max-w-screen-A4 mx-w-xl bg-white shadow-xl print:shadow-none shadow-2xl p-8 md:p-12 print:py-0 rounded-md lg:rounded-sm">
+      {children}
+      <style jsx global>{`
+        @media screen {
+          html {
+            padding-top: 56px;
+            background: linear-gradient(
+              0deg,
+              hsl(189.88deg 86.31% 62.75%) 0%,
+              hsl(162.48deg 100% 82.02%) 66.66%,
+              hsl(189.88deg 86.31% 62.75%) 100%
+            );
+            height: 200%;
           }
-          @media screen and (max-width: 768px) {
-            html {
-              padding: 16px 8px 16px 8px;
-            }
+          body {
+            overflow: scroll;
           }
-        `}</style>
-      </main>
-    </>
+        }
+        @media screen and (max-width: 768px) {
+          html {
+            padding: 16px 8px 16px 8px;
+          }
+        }
+      `}</style>
+    </main>
   );
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,19 @@ class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="Steven Gouws - Javascript Developer"
+          />
+          <title>Steven Gouws - Javascript Developer</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
