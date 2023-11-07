e6abba47083b09aabc7e789b29b7ff1026af944e
Steven Gouws <steven@stevengouws.com>
2023-11-07 11:41:28 +0000

    Remove unnecessary use of dangerouslySetInnerHTML

--- pages/typography/index.js
+++ pages/typography/index.js
@@ -2,28 +2,22 @@ import Paper from "../../components/Paper";
 import Section from "../../components/Section";
 
 export default function Typography() {
-  const headingTags = ["h1", "h2", "h3", "h4", "h5", "h6"];
-  const headingStrings = headingTags.map((tag) => {
-    return `<${tag}>A Visual Type Scale</${tag}>`;
-  });
   return (
     <Paper>
       <div>
-        {headingStrings.map((heading) => (
-          <Section key={heading}>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: heading,
-              }}
-            ></div>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-              voluptas error eum voluptate tempore repellat explicabo autem, non
-              ea alias dolores reprehenderit adipisci ipsa aut laudantium
-              ducimus! Nam, reiciendis consequuntur.
-            </p>
-          </Section>
-        ))}
+        {["h1", "h2", "h3", "h4", "h5", "h6"].map((Heading) => {
+          return (
+            <Section key={Heading}>
+              <Heading>A Visual Type Scale</Heading>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Impedit voluptas error eum voluptate tempore repellat explicabo
+                autem, non ea alias dolores reprehenderit adipisci ipsa aut
+                laudantium ducimus! Nam, reiciendis consequuntur.
+              </p>
+            </Section>
+          );
+        })}
         <Section>
           <h6>Some really small text</h6>
           <p className="text-sm">
