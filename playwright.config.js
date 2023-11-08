5534eb5e8f9cb376f2b60cd01c866c83dc08b9d2
Steven Gouws <steven@stevengouws.com>
2023-11-08 11:29:49 +0000

    For real use multiple workers for Github Actions

--- playwright.config.js
+++ playwright.config.js
@@ -20,6 +20,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 8 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   webServer: {
