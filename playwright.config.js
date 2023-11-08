edd7aed01ecdaff25c08dc4bc5c82268e871b097
Steven Gouws <steven@stevengouws.com>
2023-11-08 11:21:31 +0000

    Update Playwright config for local testing

--- playwright.config.js
+++ playwright.config.js
@@ -10,6 +10,8 @@ const { defineConfig, devices } = require("@playwright/test");
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
+const baseURL = "http://127.0.0.1:3000";
+
 module.exports = defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
@@ -25,11 +27,12 @@ module.exports = defineConfig({
   webServer: {
     command: "npm run start",
     reuseExistingServer: !process.env.CI,
+    url: baseURL,
   },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-failure",
