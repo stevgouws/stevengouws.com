d1e14a8eb79c01902828178ffb67b436c60bf2e9
Steven Gouws <steven@stevengouws.com>
2024-01-24 17:05:30 +0000

    Add Tooltips

--- containers/quotes/index.js
+++ containers/quotes/index.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Carousel from "../../components/Carousel";
 import { QuoteCard } from "./QuoteCard";
 import { quotes } from "./quote-data";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../../components/Tooltip";
 
 export default function Quotes() {
   return (
     <section className="max-w-full md:max-w-3xl">
       <h3 className="text-gray-900 font-sansAlt font-bold text-center pb-4 px-8 m-0">
         Kind words from colleagues
+        <MoreInfo />
       </h3>
       <Carousel className="md:px-8">
         {quotes.map((author) => {
@@ -24,5 +31,28 @@ export default function Quotes() {
         })}
       </Carousel>
     </section>
+  );
+}
+
+function MoreInfo() {
+  const [showTooltip, setShowTooltip] = useState(false);
+  return (
+    <Tooltip open={showTooltip} onOpenChange={setShowTooltip}>
+      <TooltipTrigger
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+        style={{
+          fontSize: "1rem",
+          color: "red",
+          cursor: "help",
+          position: "absolute",
+        }}
+      >
+        *
+      </TooltipTrigger>
+      <TooltipContent>
+        Extracts from our last review cycle at Fika
+      </TooltipContent>
+    </Tooltip>
   );
 }
